183a254266da6c26feca63e1f2921af172a19fbf
Mmihaimurtaza <45435470+Mmihaimurtaza@users.noreply.github.com>
2025-01-20 22:37:58 +0200

    Update matchesView.js

--- src/views/matchesView.js
+++ src/views/matchesView.js
@@ -11,7 +11,7 @@ class MatchesView extends View {
            //return 10 games 
             return `
                 
-             ${matches.slice((matchWeek*10)-10, 379 ).map(this._generateMarkupMatch.bind(this)).join('')}
+             ${matches.slice((matchWeek*10)-10, (matchWeek*10)-2 ).map(this._generateMarkupMatch.bind(this)).join('')}
         
             `;
           
